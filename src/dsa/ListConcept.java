f63b398035df2fa8193a791e06221354486a4b1a
Arabinda Mohanty <arabindamohanty@Arabindas-MacBook-Air.local>
2024-08-04 17:10:35 +0530

    dsa added

---
+++ src/dsa/ListConcept.java
@@ -0,0 +1,14 @@
+package dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListConcept {
+	public static void main(String[] args) {
+		
+	
+	List a = new ArrayList();
+	a.add("hello");
+
+}
+}
