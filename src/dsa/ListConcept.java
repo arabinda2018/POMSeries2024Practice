00630391edac551b4d90c35852881c39dc2eda7f
Arabinda Mohanty <arabindamohanty@Arabindas-MacBook-Air.local>
2024-08-04 17:58:02 +0530

    Updated as per review comment

--- src/dsa/ListConcept.java
+++ src/dsa/ListConcept.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListConcept {
-	public static void main(String[] args) {
-		
-	
-	List a = new ArrayList();
-	a.add("hello");
-
+	public static void main(String[] args) {	
+	Object user=	getArrayElement("hello");
+	System.out.println(user);
 }
+	public static Object getArrayElement(Object user) {
+		
+		List a = new ArrayList();
+		a.add("hello");
+		a.add("hi");
+		a.add("Core java");
+		return a.add("Selenium");
+		
+	}
 }
